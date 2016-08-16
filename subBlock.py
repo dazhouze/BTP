03daaae00142157f21018e344845a4a4176284c1
zhouze <dazhouze@link.cuhk.edu.hk>
2016-08-16 15:53:17 +0800

    Test SSH connection.

--- subBlock.py
+++ subBlock.py
@@ -13,28 +13,30 @@ def which_win (start, end):
 
 def snp_density (array):
     densityDic = {}
+    readDic = {}
     for n in array:
         win = which_win(n.getStart(), n.getEnd())
-        snpNum = n.getSnpNum() / n.getLen()
-        try:
-            densityDic[win]
-        except:
-            densityDic[win] = 0
-        finally:
+        snpNum = n.getSnpNum()
+        if win in densityDic:
             densityDic[win] = densityDic[win] + snpNum
+            readDic[win] = readDic[win] + n.getLen()
+        else:
+            densityDic[win] = snpNum
+            readDic[win] = n.getLen()
 
     for k in sorted(densityDic):
         start = k * winSize + 1
         end = (k + 1) * winSize
-        print('hs6\t%d\t%d\t%d'% (start, end, densityDic[k]))
+        value = densityDic[k] / readDic[k] * winSize
+        print('hs6\t%d\t%d\t%d'% (start, end, value))
 
 
 
 
 def get_snp ():
     #read array more detail in callSnp.py
-    readSnp = callSnp.openFile ('/home/zhouze/team/YH05_PacBio/YH06_YH07/2_bestHit/YH14567.best.sorted.bam', 28477797, 33448354)
-    #readSnp = callSnp.openFile ('/home/zhouze/team/YH05_PacBio/YH06_YH07/2_bestHit/YH14567.best.sorted.bam', 29910247, 29913661)
+    readSnp = callSnp.openFile ('/home/zhouze/team/YH05_PacBio/YH06_YH07/2_bestHit/YH14567.best.sorted.bam', 28477797, 33448354)#whole HMC region
+    #readSnp = callSnp.openFile ('/home/zhouze/team/YH05_PacBio/YH06_YH07/2_bestHit/YH14567.best.sorted.bam', 29910247, 29913661)#part HMC region
     #print('reads num: ', len(readSnp))
     snp_density(readSnp)
 
