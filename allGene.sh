af1432ec6bb0b5de6e15b3eab363fc5c97b5e226
zhouze <dazhouze@link.cuhk.edu.hk>
2016-09-13 17:43:49 +0800

    Change bwa paramters.

--- allGene.sh
+++ allGene.sh
@@ -96,8 +96,8 @@ samtools index DRB1/DRB1.1.fastq.bam
 ##Gene A
 canu  useGrid=false -p A -d A/A_0 genomesize=6000 -pacbio-raw  A/phase.0.qname.fastq
 canu  useGrid=false -p A -d A/A_1 genomesize=6000 -pacbio-raw  A/phase.1.qname.fastq
-bwa mem -x pacbio ~/team/dataBase/hg19_chr6/chr6.fa A/A_0/A.consensus.fasta > A/A.0.contig.sam
-bwa mem -x pacbio ~/team/dataBase/hg19_chr6/chr6.fa A/A_1/A.consensus.fasta > A/A.1.contig.sam
+bwa mem ~/team/dataBase/hg19_chr6/chr6.fa A/A_0/A.consensus.fasta > A/A.0.contig.sam
+bwa mem ~/team/dataBase/hg19_chr6/chr6.fa A/A_1/A.consensus.fasta > A/A.1.contig.sam
 samtools view -Sb A/A.1.contig.sam | samtools sort > A/A.1.contig.bam
 samtools view -Sb A/A.0.contig.sam | samtools sort > A/A.0.contig.bam
 samtools index A/A.0.contig.bam
@@ -105,8 +105,8 @@ samtools index A/A.1.contig.bam
 ##Gene B
 canu  useGrid=false -p B -d B/B_0 genomesize=6000 -pacbio-raw  B/phase.0.qname.fastq
 canu  useGrid=false -p B -d B/B_1 genomesize=6000 -pacbio-raw  B/phase.1.qname.fastq
-bwa mem -x pacbio ~/team/dataBase/hg19_chr6/chr6.fa B/B_0/B.consensus.fasta > B/B.0.contig.sam
-bwa mem -x pacbio ~/team/dataBase/hg19_chr6/chr6.fa B/B_1/B.consensus.fasta > B/B.1.contig.sam
+bwa mem ~/team/dataBase/hg19_chr6/chr6.fa B/B_0/B.consensus.fasta > B/B.0.contig.sam
+bwa mem ~/team/dataBase/hg19_chr6/chr6.fa B/B_1/B.consensus.fasta > B/B.1.contig.sam
 samtools view -Sb B/B.1.contig.sam | samtools sort > B/B.1.contig.bam
 samtools view -Sb B/B.0.contig.sam | samtools sort > B/B.0.contig.bam
 samtools index B/B.0.contig.bam
@@ -114,8 +114,8 @@ samtools index B/B.1.contig.bam
 ##Gene C
 canu  useGrid=false -p C -d C/C_0 genomesize=6000 -pacbio-raw  C/phase.0.qname.fastq
 canu  useGrid=false -p C -d C/C_1 genomesize=6000 -pacbio-raw  C/phase.1.qname.fastq
-bwa mem -x pacbio ~/team/dataBase/hg19_chr6/chr6.fa C/C_0/C.consensus.fasta > C/C.0.contig.sam
-bwa mem -x pacbio ~/team/dataBase/hg19_chr6/chr6.fa C/C_1/C.consensus.fasta > C/C.1.contig.sam
+bwa mem ~/team/dataBase/hg19_chr6/chr6.fa C/C_0/C.consensus.fasta > C/C.0.contig.sam
+bwa mem ~/team/dataBase/hg19_chr6/chr6.fa C/C_1/C.consensus.fasta > C/C.1.contig.sam
 samtools view -Sb C/C.1.contig.sam | samtools sort > C/C.1.contig.bam
 samtools view -Sb C/C.0.contig.sam | samtools sort > C/C.0.contig.bam
 samtools index C/C.0.contig.bam
@@ -123,8 +123,8 @@ samtools index C/C.1.contig.bam
 ##Gene DQA1
 canu  useGrid=false -p DQA1 -d DQA1/DQA1_0 genomesize=6000 -pacbio-raw  DQA1/phase.0.qname.fastq
 canu  useGrid=false -p DQA1 -d DQA1/DQA1_1 genomesize=6000 -pacbio-raw  DQA1/phase.1.qname.fastq
-bwa mem -x pacbio ~/team/dataBase/hg19_chr6/chr6.fa DQA1/DQA1_0/DQA1.consensus.fasta > DQA1/DQA1.0.contig.sam
-bwa mem -x pacbio ~/team/dataBase/hg19_chr6/chr6.fa DQA1/DQA1_1/DQA1.consensus.fasta > DQA1/DQA1.1.contig.sam
+bwa mem ~/team/dataBase/hg19_chr6/chr6.fa DQA1/DQA1_0/DQA1.consensus.fasta > DQA1/DQA1.0.contig.sam
+bwa mem ~/team/dataBase/hg19_chr6/chr6.fa DQA1/DQA1_1/DQA1.consensus.fasta > DQA1/DQA1.1.contig.sam
 samtools view -Sb DQA1/DQA1.1.contig.sam | samtools sort > DQA1/DQA1.1.contig.bam
 samtools view -Sb DQA1/DQA1.0.contig.sam | samtools sort > DQA1/DQA1.0.contig.bam
 samtools index DQA1/DQA1.0.contig.bam
@@ -132,8 +132,8 @@ samtools index DQA1/DQA1.1.contig.bam
 ##Gene DQB1
 canu  useGrid=false -p DQB1 -d DQB1/DQB1_0 genomesize=6000 -pacbio-raw  DQB1/phase.0.qname.fastq
 canu  useGrid=false -p DQB1 -d DQB1/DQB1_1 genomesize=6000 -pacbio-raw  DQB1/phase.1.qname.fastq
-bwa mem -x pacbio ~/team/dataBase/hg19_chr6/chr6.fa DQB1/DQB1_0/DQB1.consensus.fasta > DQB1/DQB1.0.contig.sam
-bwa mem -x pacbio ~/team/dataBase/hg19_chr6/chr6.fa DQB1/DQB1_1/DQB1.consensus.fasta > DQB1/DQB1.1.contig.sam
+bwa mem ~/team/dataBase/hg19_chr6/chr6.fa DQB1/DQB1_0/DQB1.consensus.fasta > DQB1/DQB1.0.contig.sam
+bwa mem ~/team/dataBase/hg19_chr6/chr6.fa DQB1/DQB1_1/DQB1.consensus.fasta > DQB1/DQB1.1.contig.sam
 samtools view -Sb DQB1/DQB1.1.contig.sam | samtools sort > DQB1/DQB1.1.contig.bam
 samtools view -Sb DQB1/DQB1.0.contig.sam | samtools sort > DQB1/DQB1.0.contig.bam
 samtools index DQB1/DQB1.0.contig.bam
@@ -141,8 +141,8 @@ samtools index DQB1/DQB1.1.contig.bam
 ##Gene DPA1
 canu  useGrid=false -p DPA1 -d DPA1/DPA1_0 genomesize=6000 -pacbio-raw  DPA1/phase.0.qname.fastq
 canu  useGrid=false -p DPA1 -d DPA1/DPA1_1 genomesize=6000 -pacbio-raw  DPA1/phase.1.qname.fastq
-bwa mem -x pacbio ~/team/dataBase/hg19_chr6/chr6.fa DPA1/DPA1_0/DPA1.consensus.fasta > DPA1/DPA1.0.contig.sam
-bwa mem -x pacbio ~/team/dataBase/hg19_chr6/chr6.fa DPA1/DPA1_1/DPA1.consensus.fasta > DPA1/DPA1.1.contig.sam
+bwa mem ~/team/dataBase/hg19_chr6/chr6.fa DPA1/DPA1_0/DPA1.consensus.fasta > DPA1/DPA1.0.contig.sam
+bwa mem ~/team/dataBase/hg19_chr6/chr6.fa DPA1/DPA1_1/DPA1.consensus.fasta > DPA1/DPA1.1.contig.sam
 samtools view -Sb DPA1/DPA1.1.contig.sam | samtools sort > DPA1/DPA1.1.contig.bam
 samtools view -Sb DPA1/DPA1.0.contig.sam | samtools sort > DPA1/DPA1.0.contig.bam
 samtools index DPA1/DPA1.0.contig.bam
@@ -150,8 +150,8 @@ samtools index DPA1/DPA1.1.contig.bam
 ##Gene DPB1
 canu  useGrid=false -p DPB1 -d DPB1/DPB1_0 genomesize=6000 -pacbio-raw  DPB1/phase.0.qname.fastq
 canu  useGrid=false -p DPB1 -d DPB1/DPB1_1 genomesize=6000 -pacbio-raw  DPB1/phase.1.qname.fastq
-bwa mem -x pacbio ~/team/dataBase/hg19_chr6/chr6.fa DPB1/DPB1_0/DPB1.consensus.fasta > DPB1/DPB1.0.contig.sam
-bwa mem -x pacbio ~/team/dataBase/hg19_chr6/chr6.fa DPB1/DPB1_1/DPB1.consensus.fasta > DPB1/DPB1.1.contig.sam
+bwa mem ~/team/dataBase/hg19_chr6/chr6.fa DPB1/DPB1_0/DPB1.consensus.fasta > DPB1/DPB1.0.contig.sam
+bwa mem ~/team/dataBase/hg19_chr6/chr6.fa DPB1/DPB1_1/DPB1.consensus.fasta > DPB1/DPB1.1.contig.sam
 samtools view -Sb DPB1/DPB1.1.contig.sam | samtools sort > DPB1/DPB1.1.contig.bam
 samtools view -Sb DPB1/DPB1.0.contig.sam | samtools sort > DPB1/DPB1.0.contig.bam
 samtools index DPB1/DPB1.0.contig.bam
@@ -159,8 +159,8 @@ samtools index DPB1/DPB1.1.contig.bam
 ##Gene DRB1
 canu  useGrid=false -p DRB1 -d DRB1/DRB1_0 genomesize=6000 -pacbio-raw  DRB1/phase.0.qname.fastq
 canu  useGrid=false -p DRB1 -d DRB1/DRB1_1 genomesize=6000 -pacbio-raw  DRB1/phase.1.qname.fastq
-bwa mem -x pacbio ~/team/dataBase/hg19_chr6/chr6.fa DRB1/DRB1_0/DRB1.consensus.fasta > DRB1/DRB1.0.contig.sam
-bwa mem -x pacbio ~/team/dataBase/hg19_chr6/chr6.fa DRB1/DRB1_1/DRB1.consensus.fasta > DRB1/DRB1.1.contig.sam
+bwa mem ~/team/dataBase/hg19_chr6/chr6.fa DRB1/DRB1_0/DRB1.consensus.fasta > DRB1/DRB1.0.contig.sam
+bwa mem ~/team/dataBase/hg19_chr6/chr6.fa DRB1/DRB1_1/DRB1.consensus.fasta > DRB1/DRB1.1.contig.sam
 samtools view -Sb DRB1/DRB1.1.contig.sam | samtools sort > DRB1/DRB1.1.contig.bam
 samtools view -Sb DRB1/DRB1.0.contig.sam | samtools sort > DRB1/DRB1.0.contig.bam
 samtools index DRB1/DRB1.0.contig.bam
